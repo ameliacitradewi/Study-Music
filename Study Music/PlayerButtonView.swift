1635c6bd41d335511ea36b557d52710fca4a9f10
Amelia Citra <ameliacitra@amelia-macbook.local>
2023-10-12 21:38:30 +0700

    first commit

---
+++ Study Music/PlayerButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerButtonView.swift
+//  Study Music
+//
+//  Created by Amelia Citra on 13/09/23.
+//
+
+import SwiftUI
+
+struct PlayerButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerButtonView()
+    }
+}
---
+++ Study Music/Study_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_MusicApp.swift
+//  Study Music
+//
+//  Created by Amelia Citra on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Study_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
