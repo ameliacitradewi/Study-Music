1c5dc61b65fa0dfac07bc2f805b534d30ead3115
ameliacitradewi <ameliacitradewi@gmail.com>
2023-10-29 19:48:09 +0700

    Add timer view and model

---
+++ Study Music/TimerModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimerModel.swift
+//  Study Music
+//
+//  Created by Amelia Citra on 29/10/23.
+//
+
+import Foundation
---
+++ Study Music/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  Study Music
+//
+//  Created by Amelia Citra on 29/10/23.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
