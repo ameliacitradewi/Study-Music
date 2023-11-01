ae5c360826e7964668459f48f999890bb02e6d5e
Amelia Citra <ameliacitradewi@gmail.com>
2023-11-01 22:00:00 +0700

    change timer model
    change timer model to hour:minutes:seconds with picker

--- Study Music/TimerViews.swift
+++ Study Music/TimerViews.swift
@@ -2,51 +2,116 @@
 //  TimerViews.swift
 //  Study Music
 //
-//  Created by Amelia Citra on 31/10/23.
+//  Created by Amelia Citra on 01/11/23.
 //
 
 import SwiftUI
 
-struct TimerViews: View {
-    @StateObject private var vm = ViewModel()
-    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    private let width: Double = 250
+struct CountdownTimerView: View {
+    @State private var hours = 0
+    @State private var minutes = 0
+    @State private var seconds = 0
+    @State private var isRunning = false
+    @State private var timer: Timer?
+    @State private var isPickerVisible = true
     
     var body: some View {
         VStack {
-            Text("\(vm.time)")
-                .font(.system(size: 50, weight: .medium, design: .rounded))
-                .alert("Timer done!", isPresented: $vm.showingAlert) {
-                    Button("Continue", role: .cancel) {
-                        // more code
+            
+            HStack {
+                Text(String(format: "%02d:%02d:%02d", hours, minutes, seconds))
+                    .font(.system(size: 50, weight: .medium, design: .rounded))
+                    .padding()
+            }
+            
+            if isPickerVisible {
+                HStack {
+                    Picker("Jam", selection: $hours) {
+                        ForEach(0..<24, id: \.self) { hour in
+                            Text("\(hour)")
+                        }
+                    }
+                    .frame(width: 100)
+                    .clipped()
+                    .pickerStyle(WheelPickerStyle())
+                    
+                    Picker("Menit", selection: $minutes) {
+                        ForEach(0..<60, id: \.self) { minute in
+                            Text("\(minute)")
+                        }
+                    }
+                    .frame(width: 100)
+                    .clipped()
+                    .pickerStyle(WheelPickerStyle())
+                    
+                    Picker("Detik", selection: $seconds) {
+                        ForEach(0..<60, id: \.self) { second in
+                            Text("\(second)")
+                        }
+                    }
+                    .frame(width: 100)
+                    .clipped()
+                    .pickerStyle(WheelPickerStyle())
+                }
+                .padding()
+            }
+            
+            HStack {
+                Button(isRunning ? "Pause" : "Start") {
+                    if isRunning {
+                        pauseTimer()
+                    } else {
+                        startTimer()
+                        isPickerVisible = false
                     }
                 }
-            
-            Slider(value: $vm.minute, in: 1...59, step: 1)
-                .frame(width: width)
-                .disabled(vm.isActive)
-                .animation(.easeInOut, value: vm.minute)
-            
-            HStack(spacing: 15) {
-                Button("Start") {
-                    vm.startTimer(minute: vm.minute)
+                Button("Reset") {
+                    resetTimer()
+                    isPickerVisible = true
+                    hours = 0
+                    minutes = 0
+                    seconds = 0
                 }
-                .disabled(vm.isActive)
-                
-                Button("Reset", action: vm.resetTimer)
-                    .tint(.red)
+                .disabled(!isRunning)
             }
-            .frame(width: width)
-            
         }
-        .onReceive(timer) { _ in
-            vm.updateCountdown()
+    }
+    
+    func startTimer() {
+        isRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if hours == 0, minutes == 0, seconds == 0 {
+                resetTimer()
+            } else {
+                if seconds > 0 {
+                    seconds -= 1
+                } else if minutes > 0 {
+                    minutes -= 1
+                    seconds = 59
+                } else if hours > 0 {
+                    hours -= 1
+                    minutes = 59
+                    seconds = 59
+                }
+            }
         }
+    }
+    
+    func pauseTimer() {
+        isRunning = false
+        timer?.invalidate()
+    }
+    
+    func resetTimer() {
+        isRunning = false
+        timer?.invalidate()
+        isPickerVisible = true
     }
 }
 
-struct TimerViews_Previews: PreviewProvider {
+struct CountdownTimerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimerViews()
+        CountdownTimerView()
     }
 }
+
